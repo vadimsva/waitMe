c43ebf8d6a6d53d4d300bfdbf594713ecca877f0
vadimsva <vadimsva@gmail.com>
2016-02-15 11:22:43 +0600

    15.02.16

--- waitMe.js
+++ waitMe.js
@@ -1,5 +1,5 @@
 /*
-waitMe - 1.14 [20.11.15]
+waitMe - 1.15 [15.02.16]
 Author: vadimsva
 Github: https://github.com/vadimsva/waitMe
 */
@@ -17,8 +17,6 @@ Github: https://github.com/vadimsva/waitMe
 			addStyle = '',
 			effectElemHTML = '',
 			waitMeObj,
-			containerSize,
-			elemSize,
 			_options,
 			currentID;
 
@@ -29,8 +27,7 @@ Github: https://github.com/vadimsva/waitMe
             text: '',
             bg: 'rgba(255,255,255,0.7)',
             color: '#000',
-            sizeW: '',
-            sizeH: '',
+						maxSize: '',
             source: '',
 						onClose: function() {}
           };
@@ -38,7 +35,6 @@ Github: https://github.com/vadimsva/waitMe
 
           currentID = new Date().getMilliseconds();
           waitMeObj = $('<div class="' + elemClass + '" data-waitme_id="' + currentID + '"></div>');
-          var size = 'width:' + _options.sizeW + ';height:' + _options.sizeH;
 
           switch (_options.effect) {
             case 'none':
@@ -46,56 +42,36 @@ Github: https://github.com/vadimsva/waitMe
               break;
             case 'bounce':
               effectElemCount = 3;
-              containerSize = '';
-              elemSize = size;
               break;
             case 'rotateplane':
               effectElemCount = 1;
-              containerSize = '';
-              elemSize = size;
               break;
             case 'stretch':
               effectElemCount = 5;
-              containerSize = '';
-              elemSize = size;
               break;
             case 'orbit':
               effectElemCount = 2;
-              containerSize = size;
-              elemSize = '';
               break;
             case 'roundBounce':
               effectElemCount = 12;
-              containerSize = size;
-              elemSize = '';
               break;
             case 'win8':
               effectElemCount = 5;
               createSubElem = true;
-              containerSize = size;
-              elemSize = size;
               break;
             case 'win8_linear':
               effectElemCount = 5;
               createSubElem = true;
-              containerSize = size;
-              elemSize = '';
               break;
             case 'ios':
               effectElemCount = 12;
-              containerSize = size;
-              elemSize = '';
               break;
             case 'facebook':
               effectElemCount = 3;
-              containerSize = '';
-              elemSize = size;
               break;
             case 'rotation':
               effectElemCount = 1;
               specificAttr = 'border-color';
-              containerSize = '';
-              elemSize = size;
               break;
             case 'timer':
               effectElemCount = 2;
@@ -105,44 +81,29 @@ Github: https://github.com/vadimsva/waitMe
 								var color = _options.color;
 							}
               addStyle = 'border-color:' + color;
-              containerSize = size;
-              elemSize = '';
               break;
             case 'pulse':
               effectElemCount = 1;
               specificAttr = 'border-color';
-              containerSize = '';
-              elemSize = size;
               break;
             case 'progressBar':
               effectElemCount = 1;
-              containerSize = '';
-              elemSize = size;
               break;
             case 'bouncePulse':
               effectElemCount = 3;
-              containerSize = '';
-              elemSize = size;
               break;
             case 'img':
               effectElemCount = 1;
-              containerSize = '';
-              elemSize = size;
               break;
           }
 
-          if (_options.sizeW === '' && _options.sizeH === '') {
-            elemSize = '';
-            containerSize = '';
+          if (addStyle !== '') {
+            addStyle += ';';
           }
-          if (containerSize !== '' && addStyle !== '') {
-            addStyle = ';' + addStyle;
-          }
-
+					
           if (effectElemCount > 0) {
-            effectObj = $('<div class="' + elemClass + '_progress ' + _options.effect + '"></div>');
-            if(_options.effect == 'img') {
-              effectElemHTML = '<img src="' + _options.source + '" style="' + elemSize + '">';
+            if(_options.effect === 'img') {
+							effectElemHTML = '<img src="' + _options.source + '">';
             } else {
               for (var i = 1; i <= effectElemCount; ++i) {
 								if ($.isArray(_options.color)) {
@@ -154,16 +115,16 @@ Github: https://github.com/vadimsva/waitMe
 									var color = _options.color;
 								}
                 if (createSubElem) {
-                  effectElemHTML += '<div class="' + elemClass + '_progress_elem' + i + '" style="' + elemSize + '"><div style="' + specificAttr +':' + color +'"></div></div>';
+                  effectElemHTML += '<div class="' + elemClass + '_progress_elem' + i + '"><div style="' + specificAttr +':' + color + '"></div></div>';
                 } else {
-                  effectElemHTML += '<div class="' + elemClass + '_progress_elem' + i + '" style="' + specificAttr + ':' + color +';' + elemSize + '"></div>';
+                  effectElemHTML += '<div class="' + elemClass + '_progress_elem' + i + '" style="' + specificAttr + ':' + color + '"></div>';
                 }
               }
             }
-            effectObj = $('<div class="' + elemClass + '_progress ' + _options.effect + '" style="' + containerSize + addStyle + '">' + effectElemHTML + '</div>');
+            effectObj = $('<div class="' + elemClass + '_progress ' + _options.effect + '" style="' + addStyle + '">' + effectElemHTML + '</div>');
           }
 
-          if (_options.text) {
+          if (_options.text && _options.maxSize === '') {
 						if ($.isArray(_options.color)) {
 							var color = _options.color[0];
 						} else {
@@ -186,7 +147,22 @@ Github: https://github.com/vadimsva/waitMe
 					elemObj = elem.find('> .' + elemClass);
 					var elemContentObj = elem.find('.' + elemClass + '_content');
           elemObj.css({background: _options.bg});
-          elemContentObj.css({marginTop: - elemContentObj.outerHeight() / 2 + 'px'});
+					elemContentObj.css({marginTop: - elemContentObj.outerHeight() / 2 + 'px'});
+					
+					if (_options.maxSize !== '') {
+						var elemH = effectObj.outerHeight();
+						var elemW = effectObj.outerWidth();
+						var elemMax = elemH;
+						if (_options.effect === 'img') {
+							effectObj.css({height: _options.maxSize + 'px'});
+							effectObj.find('>img').css({maxHeight: '100%'});
+							elemContentObj.css({marginTop: - elemContentObj.outerHeight() / 2 + 'px'});
+						} else {
+							if (_options.maxSize < elemMax) {
+								waitMeDivObj.css({transform: 'scale(' + _options.maxSize / elemMax + ')'});	
+							}
+						}
+					}
 
 					function setElTop(getTop) {
 						elemContentObj.css({top: 'auto', transform: 'translateY(' + getTop + 'px) translateZ(0)'});
@@ -259,7 +235,7 @@ Github: https://github.com/vadimsva/waitMe
     });
 
   };
-  $(window).load(function(){
+  $(window).on('load', function(){
     $('body.waitMe_body').addClass('hideMe');
     setTimeout(function(){
       $('body.waitMe_body').find('.waitMe_container:not([data-waitme_id])').remove();
